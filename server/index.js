3e07495757148b57e9f4397a55c9707fca04a52e
rushikadam12 <rushi55578@gmail.com>
2024-03-13 15:32:57 +0530

    socket example template

--- server/index.js
+++ server/index.js
@@ -3,8 +3,42 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const DB = require("./src/DB/DBconnection");
-
+const path = require("path");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
+
+const user = {};
+app.use(express.static(path.resolve("../server/src/public")));
+
+io.on("connection", (socket) => {
+  // console.log("connection establish:", socket.id);
+
+  socket.on("room code", (code) => {
+    socket.join(code);
+    console.log("user has joined the room");
+  });
+
+  socket.on("user-name", (name) => {
+    user[socket.id] = name;
+  });
+  socket.on("message", (msg, code) => {
+    console.log(msg);
+
+    socket
+      .to(code)
+      .emit("message", { username: user[socket.id], message: msg });
+  });
+  socket.on("leave room", (code) => {
+    socket.leave(code);
+  });
+});
+app.get("/", (req, res) => {
+  return res.sendFile("../server/src/public/index.html");
+});
 
 const startServer = () => {
   server.listen(process.env.PORT || 3000, () => {
@@ -12,6 +46,8 @@ const startServer = () => {
   });
 };
 
-DB().then(() => {
-  startServer();
-}).catch((error) => [console.log(error)]);
+DB()
+  .then(() => {
+    startServer();
+  })
+  .catch((error) => [console.log(error)]);
